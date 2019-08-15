bc541012b749c7b8696d9ee93b710c8d83a82a67
apena <andre.pena@zalando.de>
2019-08-15 20:00:29 +0200

    Add bulma

--- src/common/components/App.tsx
+++ src/common/components/App.tsx
@@ -1,24 +1,18 @@
 import React from "react";
-import logo from "../../logo.svg";
-import "./App.css";
+import "../../client/styles/bulma.scss";
+import { NavBar } from "./navbar";
+import { SettingsMenu } from "./settings-menu";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <NavBar />
+      <div className="columns">
+        <div className="column is-one-quarter">
+          <SettingsMenu />
+        </div>
+        <div className="column">Second column</div>
+      </div>
     </div>
   );
 };
---
+++ src/common/components/settings.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export const Settings: React.FunctionComponent = () => {
+  return <div>Settings</div>;
+};
