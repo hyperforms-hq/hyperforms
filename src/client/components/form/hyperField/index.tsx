41651701238fbcbc524def808523b6aafd5119b4
apena <andrerpena@gmail.com>
2019-09-10 23:33:41 +0200

    Add HyperField, HyperText and HyperPassword

--- src/client/components/form/hyperField/index.tsx
+++ src/client/components/form/hyperField/index.tsx
@@ -1,24 +1,37 @@
 import * as React from "react";
 import classNames from "classnames";
+import { Field } from "react-final-form";
+import { HyperInput } from "../hyperInput";
+import { HyperPassword } from "../hyperPassword";
+
+export type HyperFieldType = "string" | "password" | "integer";
 
 export interface InputProps {
-  label: string;
-  children: React.ReactNode;
-  childrenWrapperClass?: string[];
+  name: string;
+  type: HyperFieldType;
+  label?: string;
+  required?: boolean;
 }
 
-export const HyperField: React.FunctionComponent<InputProps> = ({
-  label,
-  children,
-  childrenWrapperClass
-}) => {
+function getComponentFromProps(props: InputProps): React.ReactNode {
+  switch (props.type) {
+    case "string":
+      return <Field name={props.name} component={HyperInput} />;
+    case "password":
+      return <Field name={props.name} component={HyperPassword} />;
+    case "integer":
+      return <Field name={props.name} component={HyperInput} />;
+  }
+}
+
+export const HyperField: React.FunctionComponent<InputProps> = props => {
   return (
     <div className="field">
       <label htmlFor="" className="label">
-        {label}
+        {props.label}
       </label>
-      <div className={classNames("control", childrenWrapperClass)}>
-        {children}
+      <div className={classNames("control")}>
+        {getComponentFromProps(props)}
       </div>
     </div>
   );
--- src/client/components/pages/login/index.tsx
+++ src/client/components/pages/login/index.tsx
@@ -17,31 +17,12 @@ export const Login: React.FunctionComponent = () => {
         {({ handleSubmit }) => {
           return (
             <form onSubmit={handleSubmit}>
-              <HyperField label={"e-Mail"}>
-                <Field name={"e-mail"} component={HyperInput} />
-              </HyperField>
-              <div className="field">
-                <label htmlFor="" className="label">
-                  Password
-                </label>
-                <div className="control has-icons-left">
-                  <input
-                    type="password"
-                    placeholder="*******"
-                    className="input"
-                    required
-                  />
-                  <span className="icon is-small is-left">
-                    <i className="fa fa-lock" />
-                  </span>
-                </div>
-              </div>
-              <div className="field">
-                <label htmlFor="" className="checkbox">
-                  <input type="checkbox" />
-                  <span className={styles.rememberMeText}>Remember me</span>
-                </label>
-              </div>
+              <HyperField name={"email"} label={"e-Mail"} type={"string"} />
+              <HyperField
+                name={"password"}
+                label={"Password"}
+                type={"password"}
+              />
               <div className="buttons is-right">
                 <Link to={"/signup"} className={"button"}>
                   Create account
