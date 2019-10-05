9854cd467121d854380b10e74fadc8cad44fba8d
apena <andrerpena@gmail.com>
2019-10-05 22:25:46 +0200

    Replace bcrypt with bcryptjs

--- src/server/security/passwords.ts
+++ src/server/security/passwords.ts
@@ -1,4 +1,4 @@
-import { hash, compare } from "bcrypt";
+import { hash, compare } from "bcryptjs";
 
 export async function hashPassword(password: string): Promise<string> {
   return hash(password, 10);
