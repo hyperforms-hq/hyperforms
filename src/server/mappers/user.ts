fec6be7946141d4957f81442634aedcfbb298321
apena <andrerpena@gmail.com>
2019-11-10 13:27:35 +0100

    Workspace mapper

--- src/server/mappers/user.ts
+++ src/server/mappers/user.ts
@@ -1,9 +1,18 @@
 import { UserDb } from "../database/entity/User";
-import { User } from "../graphql/graphql-types";
+import { User, Workspace } from "../graphql/graphql-types";
+import { DbWorkspace } from "../database/entity/Workspace";
 
 export function mapDbUserToUser(user: UserDb): User {
   return {
     id: user.id,
     email: user.email
+  };
+}
+
+export function mapDbWorkspaceToWorkspace(workspace: DbWorkspace): Workspace {
+  return {
+    displayName: workspace.display_name,
+    id: workspace.id,
+    urlKey: workspace.url_key
   };
 }
