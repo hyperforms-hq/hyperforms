53d5eef7187019cfb25013c17e277d3df779bf0b
apena <andrerpena@gmail.com>
2019-11-11 07:16:18 +0100

    Empty Box component

--- src/client/components/empty-box/index.tsx
+++ src/client/components/empty-box/index.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 
 export const EmptyBox: React.FunctionComponent = () => {
-    return <div>
-        There is nothing to see here
-    </div>;
+  return <div>There is nothing to see here</div>;
 };
